4f8f3b23a88ed1372e69b44718b24337930fd0e6
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-05-09 16:51:45 -0300

    Uploading project to Github

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+
+    private GameObject cam;
+
+	void Start () {
+        this.cam = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	void Update () {
+        this.shot();
+	}
+
+    void shot() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hitted;
+            if (Physics.Raycast(this.cam.transform.position, this.cam.transform.forward, out hitted)) {
+                if (hitted.transform.gameObject.GetComponent<Rigidbody>()) {
+                    Rigidbody rb = hitted.transform.gameObject.GetComponent<Rigidbody>();
+                    rb.AddExplosionForce(250f, hitted.point, 10);
+                }
+            }
+        }
+    }
+
+}
