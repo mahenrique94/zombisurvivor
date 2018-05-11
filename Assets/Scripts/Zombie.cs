79ef6a5595181b6a934767651787d8f81749fad3
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-05-11 13:32:30 -0300

    Starting implementation of AI zombie

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    private GameObject player;
+    private bool mustFollow = true;
+
+    private void initializeAttributes()
+    {
+        this.agent = this.GetComponent<NavMeshAgent>();
+        this.player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        this.toggleMustFollow();
+    }
+
+    private void OnTriggerExit(Collider other) {
+        this.toggleMustFollow();
+    }
+
+	void Start () {
+        this.initializeAttributes();
+	}
+
+    private void toggleMustFollow() {
+        this.mustFollow = !this.mustFollow;
+        this.agent.isStopped = !this.mustFollow;
+    }
+	
+	void Update () {
+        if (mustFollow) {
+            this.agent.destination = this.player.transform.position;
+        }
+	}
+
+}
